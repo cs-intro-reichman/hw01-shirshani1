5c006c99e40245fd218a6ea9b7c9359c202d8ae7
shirshani1 <shir.shani@post.runi.ac.il>
2023-12-23 21:25:42 +0200

    homework

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,17 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int minNum= Integer.parseInt(args[0]); 
+		int maxNum= Integer.parseInt(args[1]);
+		int firtNum= ((int)(Math.random() * maxNum - minNum + 1) + minNum);
+		int secondNum= ((int)(Math.random() * maxNum - minNum + 1) + minNum);
+		int thirdNum= ((int)(Math.random() * maxNum - minNum + 1) + minNum);
+		System.out.println(firtNum);
+		System.out.println(secondNum);
+		System.out.println(thirdNum);
+		int chekingMin= Math.min(firtNum, secondNum);
+		int finalMin = Math.min(chekingMin, thirdNum);
+		System.out.println("The minimal generated number was " + finalMin);
+
 	}
 }
